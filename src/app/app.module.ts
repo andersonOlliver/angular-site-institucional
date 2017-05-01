6c12ea1b6ddf4ea3d71175e386211532a5e8abc1
Anderson Olliver <pla.olliver@gmail.com>
2017-05-01 13:34:32 -0400

    adicionado rota contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { AppComponent } from './app.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
 import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
+import {RouterModule} from "@angular/router";
+import {ContatoModule} from "./contato/contato.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     QuemSomosComponent,
-    ContatoComponent,
     NossaVisaoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    ContatoModule,
+    RouterModule.forRoot([
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
