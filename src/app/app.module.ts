29b1ecb0283e38d1b9a58e86990a98380002cb50
Anderson Olliver <pla.olliver@gmail.com>
2017-05-01 12:59:15 -0400

    adicionado componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuemSomosComponent,
+    ContatoComponent,
+    NossaVisaoComponent
   ],
   imports: [
     BrowserModule,
